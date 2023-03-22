e4c57b5910dd484bbf16cb2b939747380a585485
Dmitriy <dmitriy@qlbit.com>
2023-03-21 23:51:17 -0400

    dependencies updated; rollup_riot_register.mjs plugin created

--- rollup_riot_register.mjs
+++ rollup_riot_register.mjs
@@ -2,19 +2,21 @@ import glob from 'fast-glob'
 
 export default ( paths = [], source = "riot:components" ) => {
   const camelize = ( str ) =>
-      ( ' ' + str ).toLowerCase().replace( /[-_\s.]+(.)?/g, ( _, c ) => c ? c.toUpperCase() : '' )
+    str.toLowerCase().replace( /[-_\s.]+(.)?/g, ( _, c ) => c.toUpperCase() )
   return {
-    name: source,
-    resolveId: (id) => source === id ? id : undefined,
-    load: async (id) => {
+    name: 'rollup_riot_register',
+    resolveId: ( id ) => source === id ? id : null,
+    load: async ( id ) => {
       if( source !== id )
-        return undefined
-      const imp = []
-      const reg = []
+        return null
+      const imp = [], reg = []
       for( const path of await glob( paths ) ) {
         const tokens = path.split( '/' )
-        const name = tokens[ tokens.length - 1 ].replace( /\.riot$/, '' )
-        const cname = camelize( name )
+        const last = tokens[ tokens.length - 1 ]
+        const name = last.replace( /\.riot$/i, '' )
+        if( last.length == name.length )
+          continue
+        const cname = camelize( ' ' + name )
         imp.push( `import ${ cname } from '${ path }'` )
         reg.push( `\n  register( '${ name }', ${ cname } )` )
       }
